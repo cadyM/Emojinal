cb5c0816e2c376141e4d48aacf9b61a6cd277b0b
Apple <apple@Apples-MacBook-Pro-46.local>
2019-07-12 11:33:32 -0500

    Iteration 2

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func showMessage (sender : UIButton){
+        let alertController = UIAlertController (title : "Hi", message : "Hi" , preferredStyle: UIAlertController.Style.alert )
+        alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
